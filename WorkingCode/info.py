0b344e4193825a868c3ec6699436d2f2e3a0a746
Vaibhav3835 <vaibhavraopalri@gmail.com>
2021-05-31 11:13:50 +0530

    InitialCommit

---
+++ WorkingCode/info.py
@@ -0,0 +1,85 @@
+
+def history1():
+    # text1='''Parkinson’s disease is a disorder that affects small regions in the brain that control movement, posture and balance. It is a complex disease that has many different symptoms, so that not everyone with the condition suffers from the same problems. Parkinson’s disease is named after the British doctor who wrote the first book about the disease, in 1817,that made it an easily recognized entity.'''
+    text1 = '''Parkinson’s disease is a disorder that affects small regions in the brain that control movement, posture and balance. It is a complex disease that has many different symptoms, so that not everyone with the condition suffers from the same problems. Parkinson’s disease is named after the British doctor who wrote the first book about the disease, in 1817, that made it an easily recognized entity. Parkinson called it, “The Shaking palsy,” or “paralysis agitans.”
+ Early medicines of Parkinson's illness depended on exact perception, and anticholinergic medications were utilized as ahead of schedule as the nineteenth century
+'''
+    return text1
+
+
+def symptom1():
+    text1 = '''•Tremor. A tremor, or shaking, usually begins in a limb, often your hand or fingers. You may rub your thumb and forefinger back and forth, known as a pill-rolling tremor. Your hand may tremble when it's at rest.
+
+•Slowed movement (bradykinesia). Over time, Parkinson's disease may slow your movement, making simple tasks difficult and time-consuming. Your steps may become shorter when you walk. It may be difficult to get out of a chair. You may drag your feet as you try to walk.
+
+•Rigid muscles. Muscle stiffness may occur in any part of your body. The stiff muscles can be painful and limit your range of motion.
+
+•Impaired posture and balance. Your posture may become stooped, or you may have balance problems as a result of Parkinson's disease.
+
+•Loss of automatic movements. You may have a decreased ability to perform unconscious movements, including blinking, smiling or swinging your arms when you walk.
+
+•Speech changes. You may speak softly, quickly, slur or hesitate before talking. Your speech may be more of a monotone rather than have the usual inflections.
+
+•Writing changes. It may become hard to write, and your writing may appear small
+
+•voice changes
+'''
+    return text1
+
+
+def diagnosis1():
+    text1 = '''Although there is no cure for Parkinson's disease, medicines, surgical treatment, and other therapies can often relieve some symptoms. Diagnosis is made based on health history, a physical and neurological exam, as well as a review of signs and symptoms.
+Imaging tests, such as a CAT scan or MRI, may be used to rule out other conditions. A dopamine transporter (DAT) scan may also be used. While these tests don’t confirm Parkinson’s, they can help rule out other conditions and support the doctor’s diagnosis.
+several factors appear to play a role, including:
+
+•Genes. Researchers have identified specific genetic mutations that can cause Parkinson's disease. But these are uncommon except in rare cases with many family members affected by Parkinson's disease.
+
+•Environmental triggers. Exposure to certain toxins or environmental factors may increase the risk of later Parkinson's disease, but the risk is relatively small.
+
+•Age: Parkinson’s usually appears between the ages of 50 and 60. It only occurs before the age of 40 in 5-10 percent of cases.
+
+•Toxins: Exposure to certain toxins may increase the risk of Parkinson’s disease.
+
+•Head injury: People who experience head injuries may be more likely to develop Parkinson’s disease.
+'''
+    return text1
+
+
+def treatment1():
+    text1 = '''As such there is no particular or primary treatment for parkinson’s disease. The only things that are feasible are the medications to reduce the severity of the present symptoms.
+Medicines for Parkinson's Disease
+Medicines prescribed for Parkinson's include:
+
+•Drugs that increase the level of dopamine in the brain: Levedopa. Levodopa is the most common treatment for Parkinson’s. It helps to replenish dopamine. About 75 percent of cases respond to levodopa, but not all symptoms are improved. Levodopa isgenerally given with carbidopa.
+
+•Amantadine (Symmetrel) can also be used along with levodopa.It offers short-term relief for the involuntary movements (dyskinesia) that can be a side effect of levodopa.
+
+•Anticholinergics :Anticholinergics are used to block the parasympathetic nervous system. They can help with rigidity.
+Benztropine (Cogentin) and trihexyphenidyl are anticholinergics used to treat Parkinson’s.
+
+Surgery treatements include:
+1.Deep brain simulation
+2.Pump delivered therapy
+
+'''
+    return text1
+
+
+def statistics1():
+    text1 = '''•More than 10 million people worldwide are living with PD.
+
+•Incidence of Parkinson’s disease increases with age, but an estimated four percent of people with PD are diagnosed before age 50.
+
+•Men are 1.5 times more likely to have Parkinson's disease than women.
+
+•In 2016, 6.1 million (95% uncertainty interval [UI] 5.0–7.3) individuals had Parkinson's disease globally, compared with 2.5 million (2.0–3.0) in 1990.
+
+•This increase was not solely due to increasing numbers of older people, because age-standardized prevalence rates increased by 21.7% (95% UI 18.1–25.3) over the same period (compared with an increase of 74.3%, 95% UI 69.2–79.6, for crude prevalence rates).
+
+•Parkinson's disease caused 3.2 million (95% UI 2.6–4.0) DALYs and 211 296 deaths (95% UI 167 771–265 160) in 2016.
+
+•The male-to-female ratios of age-standardized prevalence rates were similar in 2016 (1.40, 95% UI 1.36–1.43) and 1990 (1.37, 1.34–1.40).
+
+•From 1990 to 2016, age-standardized prevalence, DALY rates, and death rates increased for all global burden of disease regions except for southern Latin America, eastern Europe, and Oceania. In addition, age-standardized DALY rates generally increased across the Socio-demographic Index.
+'''
+    return text1
